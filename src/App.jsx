14adb88d9ce0b99b90b1351da9377443a9a72fc4
yasaswini1408 <yasaswinisamala2005@gmail.com>
2026-06-16 20:30:33 +0530

    feat: add Skills component to showcase programming languages, tools, and technologies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar.jsx"
 import Home from "./components/Home.jsx"
 import About from "./components/About.jsx"
+import Skills from "./components/Skills.jsx"
 function App() {
   return(
     <div className="bg-[#0d0d1a] text-white min-h-screen pt-20">
       <Navbar></Navbar>
       <Home></Home>
       <About></About>
+      <Skills></Skills>
     </div>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,54 @@
+function Skills(){
+    return(
+        <section>
+            <div>
+                <h2>Skills & Tools</h2>
+                <div className="flex flex-wrap gap-3">
+                    <p>Languages</p>
+                    <div className="flex flex-wrap gap-3">
+                        <span>C++</span>
+                        <span>C</span>
+                        <span>Python</span>
+                        <span>Javascript</span>
+                    </div>
+                </div>
+                <div className="flex flex-wrap gap-3">
+                    <p>Frontend</p>
+                    <div className="flex flex-wrap gap-3">
+                        <span>HTML5</span>
+                        <span>CSS</span>
+                        <span>ReactJS</span>
+                        <span>Javascript</span>
+                    </div>
+                </div>
+                <div className="flex flex-wrap gap-3">
+                    <p>Backend</p>
+                    <div className="flex flex-wrap gap-3">
+                        <span>ExpressJS</span>
+                        <span>NodeJS</span>
+                        <span>RestAPI's</span>
+                    </div>
+                </div>
+                <div className="flex flex-wrap gap-3">
+                    <p>Database</p>
+                    <div className="flex flex-wrap gap-3">
+                        <span>MongoDB</span>
+                        <span>SQL</span>
+                    </div>
+                </div>
+                <div className="flex flex-wrap gap-3">
+                    <p>Tools</p>
+                    <div className="flex flex-wrap gap-3">
+                        <span>Git</span>
+                        <span>GitHub</span>
+                        <span>Postman</span>
+                        <span>VSCode</span>
+                        <span>Figma</span>
+                        <span>Vercel</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+export default Skills;
